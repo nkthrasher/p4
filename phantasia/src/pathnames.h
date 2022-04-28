3be5c45d1bc53580a3d52a0a01f5d0944a212c3b
nct <nkthrasher@gmail.com>
2022-04-27 20:43:58 -0400

    Initial checkin

---
+++ phantasia/src/pathnames.h
@@ -0,0 +1,83 @@
+/*-
+ * Copyright (c) 1989, 1993
+ *      The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by the University of
+ *      California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *      @(#)pathnames.h 8.2 (Berkeley) 4/2/94
+ */
+
+/* Setting Files */
+#define CHARSTATS_FILE		"phantasia/data/charstats"
+#define MONSTER_FILE		"phantasia/data/monsters"
+#define MOTD_FILE		"phantasia/data/motd"
+#define PROTECTED_FILE		"phantasia/data/protected"
+#define SHOPITEMS_FILE		"phantasia/data/shopitems"
+#define WIZARD_FILE		"phantasia/data/wizards"
+
+/* Data Files */
+#define ACCOUNT_FILE		"phantasia/data/account"
+#define TEMP_ACCOUNT_FILE	"phantasia/data/accounts.tmp"
+#define NETWORK_FILE		"phantasia/data/networks"
+#define TEMP_NETWORK_FILE	"phantasia/data/networks.tmp"
+#define BACKUP_FILE		"phantasia/data/backup"
+#define TEMP_BACKUP_FILE	"phantasia/data/backup.tmp"
+#define CHARACTER_FILE		"phantasia/data/characters"
+#define TEMP_CHARACTER_FILE	"phantasia/data/characters.tmp"
+#define SECONDARY_CHAR_FILE	"phantasia/data/characters.2"
+#define DATA_FILE		"phantasia/data/realmdata"
+#define SCOREBOARD_FILE		"phantasia/data/scoreboard"
+#define TEMP_SCOREBOARD_FILE	"phantasia/data/scoreboard.tmp"
+#define OLD_SCOREBOARD_FILE	"phantasia/data/scoreboard.old"
+#define TAG_FILE		"phantasia/data/tags"
+#define TEMP_TAG_FILE		"phantasia/data/tags.tmp"
+#define TAGGED_FILE		"phantasia/data/tagged"
+#define TEMP_TAGGED_FILE	"phantasia/data/tagged.tmp"
+#define HISTORY_FILE		"phantasia/data/history"
+
+/* Log Files */
+#define BATTLE_LOG		"phantasia/log/battles"
+#define CANTRIP_LOG		"phantasia/log/cantrip"
+#define CHAT_LOG		"phantasia/log/chatlog"
+#define COMBAT_LOG		"phantasia/log/combat"
+#define CONNECTION_LOG		"phantasia/log/connections"
+#define DEBUG_LOG		"phantasia/log/debug"
+#define ERROR_LOG		"phantasia/log/errorlog"
+#define EVENT_LOG		"phantasia/log/events"
+#define GAME_LOG		"phantasia/log/games"
+#define HACK_LOG		"phantasia/log/hacks"
+#define LEVEL_LOG		"phantasia/log/levels"
+#define PURCHASE_LOG		"phantasia/log/purchase"
+#define SERVER_LOG		"phantasia/log/server"
+
+/* Support Files */
+#define NEW_ACCOUNT_SCRIPT "phantasia/bin/new_account_mail.pl"
+#define	ACCOUNT_PASSWORD_RESET_SCRIPT "phantasia/bin/account_reset_mail.pl"
+#define	CHARACTER_PASSWORD_RESET_SCRIPT "phantasia/bin/character_reset_mail.pl"
+
